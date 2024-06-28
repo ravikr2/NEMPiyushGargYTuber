85afdba2d8c37c11ead1b1a684403feb7bad4f98
ravikr2 <batmanxjoker2@gmail.com>
2024-06-28 23:46:09 +0530

    video7

--- video7/index.js
+++ video7/index.js
@@ -1,3 +1,29 @@
 const http=require("http")
+const fs=require("fs")
 
-console.log(http.createServer)
+const myServer=http.createServer((req,res)=>{
+    console.log("url",req.url)
+    const date=new Date()
+    const kahaSeData=req.url
+    const data=`${(date.getHours())}:${(date.getMinutes())}:${(date.getSeconds())} Par ${kahaSeData} se Data aaya\n`
+     fs.appendFile("./data.txt",data,(err,res)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log(res)
+        }
+     })
+    switch(req.url){
+        case "/": res.end("Home Page Hai Bhai")
+        return
+        case "/about" :res.end("About Page Hai Bhai")
+        return 
+        default : res.end("404 Not Found Bhai")
+        return
+    }
+})
+
+
+myServer.listen(3500,()=>{
+    console.log("server Started")
+})
